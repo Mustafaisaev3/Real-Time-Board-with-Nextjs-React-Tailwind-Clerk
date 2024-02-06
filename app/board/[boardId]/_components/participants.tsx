5804fedecdd08a4651bcb700af7d494d5dc40e75
Mustafa Isaev <mustafaisaev3@gmail.com>
2024-02-06 11:57:43 +0300

    Making Liveblocks setup

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -9,3 +9,10 @@ const Participants = () => {
 }
 
 export default Participants
+
+Participants.Skeleton = () => {
+  return (
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]" />
+  );
+};
+
