0729c1853f506f303ac60c9e7b4bd89e40255f28
Mustafa Isaev <mustafaisaev3@gmail.com>
2024-02-12 16:09:36 +0300

    Insert Layer

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,14 +1,18 @@
 'use client'
 
 import { useCallback, useState } from 'react'
-
-import { Camera, CanvasMode, CanvasState } from '@/types/canvas'
+import { nanoid } from 'nanoid'
+import { Camera, CanvasMode, CanvasState, Color, LayerType, Point } from '@/types/canvas'
 import Info from './info'
 import Participants from './participants'
 import Toolbar from './toolbar'
 import { CursorsPresence } from './cursors-presence'
-import { useCanRedo, useCanUndo, useHistory, useMutation } from '@/liveblocks.config'
+import { useCanRedo, useCanUndo, useHistory, useMutation, useStorage } from '@/liveblocks.config'
 import { pointerEventToCanvasPoint } from '@/lib/utils'
+import { LiveObject } from '@liveblocks/client'
+import LayerPreview from './layer-preview'
+
+const MAX_LAYERS = 100
 
 interface CanvasProps {
   boardId: string;
@@ -17,34 +21,92 @@ interface CanvasProps {
 export const Canvas = ({
   boardId,
 }: CanvasProps) => {
+  const layerIds = useStorage((root) => root.layerIds)
+
   const [canvasState, setCanvasState] = useState<CanvasState>({
     mode: CanvasMode.None
   })
-  const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
+
+  const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 })
+  const [lastUsedColor, setLastUsedColor] = useState<Color>({
+    r: 0,
+    g: 0,
+    b: 0,
+  })
+
 
   const history = useHistory()
   const canUndo = useCanUndo()
   const canRedo = useCanRedo()
 
+  const insertLayer = useMutation((
+    { storage, setMyPresence },
+    layerType: LayerType.Ellipse | LayerType.Rectangle | LayerType.Text | LayerType.Note,
+    position: Point,
+  ) => {
+    const liveLayers = storage.get("layers");
+    if (liveLayers.size >= MAX_LAYERS) {
+      return;
+    }
+
+    const liveLayerIds = storage.get("layerIds");
+    const layerId = nanoid();
+    const layer = new LiveObject({
+      type: layerType,
+      x: position.x,
+      y: position.y,
+      height: 100,
+      width: 100,
+      fill: lastUsedColor,
+    });
+
+    liveLayerIds.push(layerId);
+    liveLayers.set(layerId, layer);
+
+    setMyPresence({ selection: [layerId] }, { addToHistory: true });
+    setCanvasState({ mode: CanvasMode.None });
+  }, [lastUsedColor]);
+
   const onPointerMove = useMutation(({ setMyPresence }, e: React.PointerEvent) => {
     e.preventDefault()
     const current = pointerEventToCanvasPoint(e, camera)
-
-    // console.log(current)
 
     setMyPresence({cursor: current})
   }, [])
 
   const onPointerLeave = useMutation(({ setMyPresence }) => {
     setMyPresence({ cursor: null });
-  }, []);
+  }, [])
+
+  const onPointerUp = useMutation((
+    {},
+    e
+  ) => {
+    const point = pointerEventToCanvasPoint(e, camera);
+
+    if (canvasState.mode === CanvasMode.Inserting) {
+      insertLayer(canvasState.layerType, point);
+    } else {
+      setCanvasState({
+        mode: CanvasMode.None,
+      });
+    }
+
+    history.resume();
+  }, 
+  [
+    camera,
+    canvasState,
+    history,
+    insertLayer,
+  ]);
 
   const onWheel = useCallback((e: React.WheelEvent) => {
     setCamera((camera) => ({
       x: camera.x - e.deltaX,
       y: camera.y - e.deltaY,
     }));
-  }, []);
+  }, [])
 
   return (
     <main className='h-full w-full relative bg-neutral-100 touch-none'>
@@ -63,8 +125,21 @@ export const Canvas = ({
         onWheel={onWheel}
         onPointerMove={onPointerMove}
         onPointerLeave={onPointerLeave}
+        onPointerUp={onPointerUp}
       >
-        <g>
+        <g
+          style={{
+            transform: `translate(${camera.x}px, ${camera.y}px)`
+          }}
+        >
+          {layerIds.map((layerId) => (
+            <LayerPreview
+              key={layerId}
+              id={layerId}
+              onLayerPointerDown={() => {}}
+              selectionColor={'#000'}
+            />
+          ))}
           <CursorsPresence />
         </g>
       </svg>
